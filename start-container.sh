a84ab7322510bd0156a97bee48d9f96ab29c6b29
antipro <antipro@163.com>
2022-10-27 19:39:52 +0800

    启动容器时立即启动hadoop

--- start-container.sh
+++ start-container.sh
@@ -9,7 +9,7 @@ docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 docker run -itd \
                 --net=hadoop \
-                -p 50070:50070 \
+                -p 9870:9870 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
@@ -31,4 +31,4 @@ do
 done 
 
 # get into hadoop master container
-docker exec -it hadoop-master bash
+# docker exec -it hadoop-master bash
